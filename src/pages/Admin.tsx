17ca307e7f9704d59a2b2ec0ef76f64f97050a71
capycpp <sadiqliulvi77@gmail.com>
2026-09-01 13:31:28 +0400

    idk

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
+import AdminPanel from './admin/Admin'
 
 export default function Admin() {
-  return (
-    <div>
-      <h2 className="text-2xl font-semibold mb-4">Admin Dashboard</h2>
-      <p>Manage users, formulas, problems, exams and site settings.</p>
-    </div>
-  )
+  return <AdminPanel />
 }
